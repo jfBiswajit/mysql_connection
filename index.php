c658ca4bc6cffb5f1b3aaffec2b587210b8f58b3
Biswajit Biswas <jfbiswajit@gmail.com>
2020-01-16 11:00:12 +0600

    parm added

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $server = "localhost";
 $user = "root";
 $pass = "toor";
+$db = 'sandbox';
 
-$conn = new mysqli($server, $user, $pass);
+$conn = new mysqli($server, $user, $pass, $db);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
 $conn->close();
-?>
